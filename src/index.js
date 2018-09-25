ef43893513396b0734bc0bf739c358014c2f9cf7
Arkadz <penkratpavel@gmail.com>
2018-09-25 22:36:40 +0300

    index.js changing

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ module.exports = function solveEquation(equation) {
   const a = parseInt(modifyEquation[0]);
   const b = parseInt(modifyEquation[4]) * (modifyEquation[3] == '+' ? 1 : -1) / a;
   const c = parseInt(modifyEquation[8]) * (modifyEquation[7] == '+' ? 1 : -1) / a;
-   const dscrSqrt = Math.sqrt(b*b - 4*c);
-   const x1 = ((b*(-1) + dscrSqrt) / 2);
-   const x2 = ((b*(-1) - dscrSqrt) / 2);
-   if(x1 < x2) {
+  const dscrSqrt = Math.sqrt(b*b - 4*c);
+  const x1 = ((b*(-1) + dscrSqrt) / 2);
+  const x2 = ((b*(-1) - dscrSqrt) / 2);
+  if(x1 < x2) {
     return [x1, x2];
   } else {
     return [x2, x1];
